a71a65f49f54df7fb48e4eb1d5d17a194b4ff42f
Kaspar Pedaja <kaspar.pedaja@voco.ee>
2023-02-14 15:02:52 +0200

    Added handlebars template engine

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 
+const path = require('path')
+// add template engine
+const hbs = require('express-handlebars')
+// setup template engine directory and files extensions
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine('hbs', hbs.engine({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/views/layouts',
+}))
+
 // parse requests of content-type - application/json
 app.use(express.json());
 // parse requests of content_type - application/x-www-form-urlencoded
